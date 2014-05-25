056bf30b203a9d285693be1a01b11912b83909ac
sameerpatil <sameerpatil@yahoo.com>
2014-05-26 00:54:15 +0530

    New commit for a button label change

--- sameer-patil.appspot.com/src/org/sam/pat/client/Sgp1.java
+++ sameer-patil.appspot.com/src/org/sam/pat/client/Sgp1.java
@@ -41,7 +41,7 @@ public class Sgp1 implements EntryPoint {
 	public void onModuleLoad() {
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		nameField.setText("GWT User (new)");
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
